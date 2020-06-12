f8baca62d129b47e46d6a3701a558a4334029427
Jeff Bai <j.bai@wustl.edu>
2020-06-12 20:55:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,10 +21,15 @@ class Scraper
     #uses nokogiri and open-uri to grab entire document from web page
     doc =
     Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
+
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
   end
 
   def get_courses
-    doc.css(".post").first.css(".date").text
   end
 
   def make_courses
