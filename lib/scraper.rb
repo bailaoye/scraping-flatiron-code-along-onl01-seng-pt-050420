16d1f603214c439b4ea61810ca5ff861a40e73cf
Jeff Bai <j.bai@wustl.edu>
2020-06-12 20:51:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,6 +18,9 @@ class Scraper
   end
 
   def get_page
+    #uses nokogiri and open-uri to grab entire document from web page
+    doc =
+    Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
   end
 
   def get_courses
